796ef45615caaa0aac698c6796499fa097c9139a
ChiisanaYume <faintdream@163.com>
2023-06-28 16:25:21 +0800

    Commit at faintdream

--- gui-test-javafx/src/main/java/module-info.java
+++ gui-test-javafx/src/main/java/module-info.java
@@ -4,9 +4,10 @@ module guiTestJavafx {
     // 依赖的模块
     requires javafx.controls;
     requires javafx.fxml;
+
     // 导出的包
     exports com.faintdream.test1.gui.javafx;
 
-    //将指定的包打开给其他模块(javafx.fxml)访问
+    // 将指定的包打开给其他模块(javafx.fxml)访问
     opens com.faintdream.test1.gui.javafx to javafx.fxml;
 }
