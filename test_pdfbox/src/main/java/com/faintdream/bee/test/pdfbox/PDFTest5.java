5876d50062a2a847933cebfa844a13ac102fa623
ChiisanaYume <faintdream@163.com>
2023-07-07 16:59:34 +0800

    测试

--- test_pdfbox/src/main/java/com/faintdream/bee/test/pdfbox/PDFTest5.java
+++ test_pdfbox/src/main/java/com/faintdream/bee/test/pdfbox/PDFTest5.java
@@ -9,6 +9,7 @@ public class PDFTest5 {
     @Test
     public void test() throws IOException {
         ImagePdf pdf = new ImagePdfImpl();
-        pdf.pdfByImage(IOUtil.getPathF("akuma.png"),"PDFTest5.pdf");
+        // pdf.pdfByImage(IOUtil.getPathF("akuma.png"),"PDFTest5.pdf");
+        pdf.pdfByImage(IOUtil.getPathF("iria.jpg"),"PDFTest5-2.pdf");
     }
 }
