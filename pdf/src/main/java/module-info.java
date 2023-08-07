8b902225a13e0f624fb082358b2858e103ab26b0
ChiisanaYume <faintdream@163.com>
2023-08-07 13:59:55 +0800

    update

--- pdf/src/main/java/module-info.java
+++ pdf/src/main/java/module-info.java
@@ -3,7 +3,8 @@ open module pdf {
     // 依赖的模块
     requires java.base;
     requires java.desktop;
-    requires tool;
+    //requires tool;
     requires junit;
     requires org.apache.pdfbox;
+    requires tool;
 }
