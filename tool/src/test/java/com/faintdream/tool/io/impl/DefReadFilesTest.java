33e5492f001bcdb69d2a669bad8357ba229ede02
ChiisanaYume <faintdream@163.com>
2023-08-12 15:43:59 +0800

    update

--- tool/src/test/java/com/faintdream/tool/io/impl/DefReadFilesTest.java
+++ tool/src/test/java/com/faintdream/tool/io/impl/DefReadFilesTest.java
@@ -1,5 +1,6 @@
 package com.faintdream.tool.io.impl;
 
+import com.faintdream.tool.io.ReadTextFromInputStreamAble;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -10,10 +11,12 @@ import static org.junit.Assert.*;
 public class DefReadFilesTest {
 
     private DefReadFiles drf = new DefReadFiles();
+    private ReadTextFromInputStreamAble rs = new ReadTextFromInputStream();
     @Test
     public void readFileByAbsoluteTest() throws IOException {
         InputStream inputStream = drf.readByAbsolutePath("D:\\study\\BeeEase\\readme.md");
-        ReadTextFromInputStream01.read(inputStream);
+        String text = rs.readText(inputStream);
+        System.out.println(text);
     }
 
 }
